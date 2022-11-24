4c179d5482ef713c3180f6c82ec292547c49e3dd
Wein4ik <il.stafeev@yandex.ru>
2022-11-24 16:50:48 +0300

    Change db and add admin panel

--- online_store/store/admin.py
+++ online_store/store/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price',
+                    'image', 'avail', 'size', 'brand', 'type',
+                    'country', 'gender', 'color', 'article')
+    list_filter = ('type', 'size', 'brand', 'gender')
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Cat_types)
+class Cat_typesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cat')
+
+
+@admin.register(Shoe_sizes)
+class Shoe_sizesAdmin(admin.ModelAdmin):
+    list_display = ('value',)
+    list_filter = ('value',)
+
+
+@admin.register(Countries)
+class Countries(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Genders)
+class Countries(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Colors)
+class Countries(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Product, ProductsAdmin)
--- online_store/store/models.py
+++ online_store/store/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-import random
+#import random
 
 
-class Products(models.Model):
+class Product(models.Model):
+    """Товары"""
     # def gen_article(self):
     #     l = self.color[:2].upper()
     #     l += str(random.randint(1000, 9999))
@@ -21,37 +22,88 @@ class Products(models.Model):
     gender = models.ForeignKey('Genders', on_delete=models.CASCADE)
     color = models.ForeignKey('Colors', on_delete=models.CASCADE)
     article = models.CharField(max_length=50, default='1')
+    url = models.SlugField(max_length=160, unique=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 
 class Categories(models.Model):
+    """Категории"""
     name = models.CharField(max_length=255)
+    url = models.SlugField(max_length=160, unique=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Cat_types(models.Model):
+    """Типы категорий"""
     name = models.CharField(max_length=255)
     cat = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    url = models.SlugField(max_length=160, unique=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Тип категории'
+        verbose_name_plural = 'Типы категорий'
 
 
 # class Clothing_sizes(models.Model):
 #     value = models.CharField(max_length=5)
 
 class Shoe_sizes(models.Model):
+    """Размеры обуви"""
     value = models.CharField(max_length=5)
+
+    class Meta:
+        verbose_name = 'Размер обуви'
+        verbose_name_plural = 'Размеры обуви'
+        ordering = ['value']
 
 
 class Countries(models.Model):
+    """Страны производители"""
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Страна производитель'
+        verbose_name_plural = 'Страны производители'
 
 
 class Genders(models.Model):
+    """Пол"""
     name = models.CharField(max_length=7)
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Пол'
+        verbose_name_plural = 'Пол'
 
 
 class Colors(models.Model):
+    """Цвета"""
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Цвет'
+        verbose_name_plural = 'Цвета'
